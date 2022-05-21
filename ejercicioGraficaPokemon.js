8a0199a285fd116fff8fc443f82c2cd4d6470b18
dmv93 <davidmolinavacas1993@gmail.com>
2022-05-21 14:09:46 +0200

    funciona todo pero hay que meterle CSS

--- ejercicioGraficaPokemon.js
+++ ejercicioGraficaPokemon.js
@@ -1,53 +1,57 @@
 let prueba = "";
+let imag;
 function nextPage() {
 
     prueba = document.getElementById("entrada").value
 
-    
     fetch(`https://pokeapi.co/api/v2/pokemon/${prueba}`)  //Concatenar la variable prueba
         .then(res => res.json())                                        //recoger los datos de la API
         .then(json => magia(json));
 
-        function magia(url) {
-            console.log(`${url.base_experience}`)
-        
-        
+    function magia(url) {
+        //console.log(`${url.base_experience}`)
+
+
         const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Experiencia', 'Habilidad', 'Peso', 'Altura'],
-        datasets: [{
-            label: '# of Votes',
-            data: [`${url.base_experience}`, `${url.order}` ,`${url.weight}`,`${url.height}`],
-            backgroundColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)',
-                'rgba(75, 192, 192)',
-                'rgba(153, 102, 255)',
-                'rgba(255, 159, 64)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)',
-                'rgba(75, 192, 192)',
-                'rgba(153, 102, 255)',
-                'rgba(255, 159, 64x)'
-            ],
-            borderWidth: 3
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: false
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Experiencia', 'Habilidad', 'Peso', 'Altura'],
+                datasets: [{
+                    label: 'Pokemon',
+                    data: [`${url.base_experience}`, `${url.order}`, `${url.weight}`, `${url.height}`],
+                    backgroundColor: [
+                        'rgba(255, 99, 132)',
+                        'rgba(54, 162, 235)',
+                        'rgba(255, 206, 86)',
+                        'rgba(75, 192, 192)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132)',
+                        'rgba(54, 162, 235)',
+                        'rgba(255, 206, 86)',
+                        'rgba(75, 192, 192)'
+                    ],
+                    borderWidth: 3
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    }
+                }
             }
-        }
+        });
+
+        imag = url.sprites.front_default
+        console.log(imag)
+        const image = document.createElement('img')
+        image.src  = imag
+        document.querySelector('.pokemon').appendChild(image)
+
+
     }
-});
-}
 }
 
 
